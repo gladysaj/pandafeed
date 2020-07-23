ecb1db7e371ed8d8260739eba2082ba372fce19d
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-07-22 20:05:19 -0500

    feat: add footer details

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const Navbar = ({ isAuth }) => {
   };
 
   return (
-    <header className="text-gray-700 body-font">
+    <header id="up" className="text-gray-700 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
           <a href="#features" className="mr-5 hover:text-indigo-600">
